ab82f7413836c9af4511b595914e58c11746eee4
Jordi Melendez <ofineo@gmail.com>
2025-01-10 16:23:53 +0100

    Added action in js to upload to S3 with secrets

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -3,6 +3,17 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
+  // Get input values
+  const bucket = core.getInput('bucket', { required: true });
+  const bucketregion = core.getInput('bucket-region', { required: true });
+  const distFolder = core.getInput('dist-folder', { required: true });
+
+  // Upload files
+  const s3Uri = `s3://${bucket}`;
+
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --${bucketregion}`);
+
   core.notice('Hello');
 }
+
 run();
